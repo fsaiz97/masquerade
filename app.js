58d51058aaf3f391c6dfc82b3f6b84d06fb7132e
fsaiz97 <felixsaiz97@gmail.com>
2022-10-18 16:17:36 +0100

    Create basic get routes

--- app.js
+++ app.js
@@ -14,6 +14,68 @@ function setupMiddleware(app) {
 
 function addRoutes(app) {
     app.get("/", (req, res) => res.send("Welcome to the Masquerade API: We track wizards!"));
+
+    app.get("/wizard", async (req, res) => {
+        try {
+            const data = await db.query("SELECT * FROM wizard");
+            res.status(200).send(data.rows);
+        } catch (err) {
+            res.status(400).send({err: err})
+        }
+    })
+
+    app.get("/wizard/:id", async (req, res) => {
+        const id = parseInt(req.params.id);
+
+        try {
+            const data = await db.query("SELECT * FROM wizard WHERE id = $1", [id]);
+            res.status(200).send(data.rows[0]);
+        } catch (err) {
+            res.status(404).send({err: err})
+        }
+    })
+
+    app.get("/location", async (req, res) => {
+        try {
+            const data = await db.query("SELECT * FROM location")
+            res.status(200).send(data.rows);
+
+        } catch (err) {
+            res.status(400).send({err: err});
+        }
+    });
+    
+    app.get("/location/:id", async (req, res) => {
+        try {
+            const id = parseInt(req.params.id);
+            const data = await db.query("SELECT * FROM location WHERE id = $1", [id]);
+            res.status(200).send(data.rows[0]);
+            
+        } catch (err) {
+            res.status(404).send({err: err});
+        }
+    });
+    
+    app.get("/incident", async (req, res) => {
+        try {
+            const data = await db.query("SELECT * FROM incident");
+            res.status(200).send(data.rows);
+        } catch (err) {
+            res.status(400).send({err: err})
+        }
+    })
+
+    app.get("/incident/:id", async (req, res) => {
+        const id = parseInt(req.params.id);
+
+        try {
+            const data = await db.query("SELECT * FROM incident WHERE id = $1", [id]);
+            res.status(200).send(data.rows[0]);
+        } catch (err) {
+            res.status(404).send({err: err})
+        }
+    })
+    
 }
 
 setupMiddleware(app);
